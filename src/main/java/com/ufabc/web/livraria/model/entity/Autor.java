ff4ec133d49f5d952999a2f53993b1c5637249c9
Emerson Almeida Matos <emegson@live.com>
2018-08-12 18:48:55 -0300

    Remoção de bibliotecas não utilizadas

--- src/main/java/com/ufabc/web/livraria/model/entity/Autor.java
+++ src/main/java/com/ufabc/web/livraria/model/entity/Autor.java
@@ -1,13 +1,9 @@
 package com.ufabc.web.livraria.model.entity;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 
 @Entity
