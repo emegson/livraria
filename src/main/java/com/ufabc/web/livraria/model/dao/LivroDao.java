f275d28535caecc815804f888bdb65e3eecf4d7d
Rick W <ricklp@hotmail.com>
2018-08-21 00:27:40 -0300

    Add files via upload

--- src/main/java/com/ufabc/web/livraria/model/dao/LivroDao.java
+++ src/main/java/com/ufabc/web/livraria/model/dao/LivroDao.java
@@ -1,11 +1,18 @@
 package com.ufabc.web.livraria.model.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ufabc.web.livraria.model.entity.Livro;
 
 @Repository
 public interface LivroDao extends JpaRepository<Livro, Long> {
+	
+	List<Livro> findByTituloContainingIgnoreCase(String nome);	
+	Livro findByTitulo(String nome);
+	
 
 }
